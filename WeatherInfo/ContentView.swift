13bb58545f483e0434f24d0f85583843c17974c1
deshabml <deshab@yandex.ru>
2023-12-07 21:02:06 +0300

    Initial Commit

---
+++ WeatherInfo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherInfo
+//
+//  Created by Лаборатория on 07.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherInfo/WeatherInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherInfoApp.swift
+//  WeatherInfo
+//
+//  Created by Лаборатория on 07.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
