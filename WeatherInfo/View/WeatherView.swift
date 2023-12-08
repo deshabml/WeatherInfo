52880a93d5a053f915caeb0838bd99f0cec76367
deshabml <deshab@yandex.ru>
2023-12-08 11:14:07 +0300

    Added cityView

--- WeatherInfo/View/WeatherView.swift
+++ WeatherInfo/View/WeatherView.swift
@@ -12,11 +12,45 @@ struct WeatherView: View {
     @StateObject var viewModel: WeatherViewModel
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            cityView
+            Spacer()
+        }
+        .foregroundColor(.white)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            Image("Background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea())
     }
+    
 }
 
 #Preview {
     
     WeatherView(viewModel: WeatherViewModel())
+}
+
+extension WeatherView {
+
+    private var cityView: some View {
+        VStack {
+            if viewModel.isChoosingCity {
+                    TextField("Город", text: $viewModel.city)
+                        .font(.custom("AvenirNext-Bold", size: 24))
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .background(
+                            Color("DarkSea").blur(radius: 30))
+            } else {
+                Text(viewModel.weatherData?.name ?? "-")
+                    .font(.custom("AvenirNext-Bold", size: 24))
+                    .onTapGesture {
+                        viewModel.isChoosingCity.toggle()
+                    }
+            }
+        }
+        .frame(height: 50)
+    }
 }
--- WeatherInfo/ViewModel/WeatherViewModel.swift
+++ WeatherInfo/ViewModel/WeatherViewModel.swift
@@ -9,5 +9,30 @@ import Foundation
 
 final class WeatherViewModel: ObservableObject {
 
-    
+    @Published var weatherData: WeatherData?
+    @Published var city: String = "" 
+//    {
+//        didSet {
+//            checkCity(text: city)
+//        }
+//    }
+    @Published var isChoosingCity = true
+    @Published var citys: [String] = []
+    @Published var statistics: [(min: Double, max: Double, pop: Int, utc: Int)] = []
+    var isCityExists: Bool {
+        citys.count > 0
+    }
+
+    init() {
+        loadFirstCity()
+    }
+
+    func loadFirstCity() {
+        let locationService = LocationService()
+        locationService.getCityName { [unowned self] cityName in
+            city = cityName
+        }
+        print(city)
+    }
 }
+
