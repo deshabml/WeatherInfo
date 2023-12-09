7105d5184b90fc91487f4cf4300c9de204f4285c
deshabml <deshab@yandex.ru>
2023-12-09 15:02:39 +0300

    Added RealmService

---
+++ WeatherInfo/Service/RealmService.swift
@@ -0,0 +1,39 @@
+//
+//  RealmService.swift
+//  WeatherInfo
+//
+//  Created by Лаборатория on 09.12.2023.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmService {
+
+    static let shared = RealmService()
+    private let dataBase = try! Realm()
+
+    private init() { }
+
+    func createObject<T>(object: T) {
+        guard let object = object as? Object else { return }
+        do {
+            try dataBase.write {
+                dataBase.add(object)
+            }
+        } catch {
+            print("Неисправность базы данных")
+        }
+    }
+
+    func deleteObject<T>(object: T) {
+        guard let object = object as? Object else { return }
+        do {
+            try dataBase.write {
+                dataBase.delete(object)
+            }
+        } catch {
+            print("Неисправность базы данных")
+        }
+    }
+}
