a7f069b20c679af2a9e4473ad2299b8d86583f0f
deshabml <deshab@yandex.ru>
2023-12-09 14:06:16 +0300

    Added WeatherByDay Model

---
+++ WeatherInfo/Model/WeatherByDay.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherByDay.swift
+//  WeatherInfo
+//
+//  Created by Лаборатория on 09.12.2023.
+//
+
+import Foundation
+
+struct WeatherByDay {
+
+    let min: Double
+    let max: Double
+    let pop: Int
+    let utc: Int
+}
